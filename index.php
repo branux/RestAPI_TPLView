977969a87893c5d806e8b384ff2a1c8cb7f2434e
Gustavo Soriano <sorianox2010@hotmail.com>
2016-12-28 19:37:26 -0200

    Demonstração de utilização da classe Router
    Todas as requisições http serão redirecionadas a este arquivo, que vai
    tratar a url como rota com ou sem parâmetro(s)

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+	require('Router.php');
+	Router::dirStatic("restPHP");
+
+//TEMPLATES
+	Router::get('/', function (){
+		Router::make("home.html");
+	});
+
+	Router::get('/contact', function () {
+		echo 'PÁGINA CONTATO';
+	});
+
+	
+//APLICANDO TEMPLATE VIEW (este é o padrão de flags para tpl #{}# )
+ 	Router::get('/user/:nomeUser', function($data){
+		$flags    =  array("#{desenvolvedor}#" => $data["nomeUser"]);
+		Router::make("home.html",$flags);
+	});
+	
+	
+//REST API
+	Router::get('/users', function () {
+		echo 'DEVOLVER TODOS USUÁRIOS';
+	});
+
+
+	Router::post('/user', function(){
+		echo 'RECUPERANDO POSTS:';
+		print_r($_POST);
+	});
+
+
+	Router::put('/user/:nome/:idade/:cod', function($data){
+		echo 'NOVOS DADOS DO USUÁRIO PARA ALTERAÇÃO:';
+		print_r($data);
+	});
+
+	Router::delete('/user/:id', function($data){
+		echo 'APAGAR USUÁRIO PELO ID: ' . $data['id'];
+	});
